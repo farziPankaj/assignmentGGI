57e589b3ac6dc43b0d6516b9629db03156b44ad1
pankajSingh <55858450+Singh-pankaj926@users.noreply.github.com>
2023-05-01 22:57:53 +0530

    angularFE files seprately

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddDataComponent } from './add-data/add-data.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ViewDataComponent } from './view-data/view-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent},
+  { path: 'homePage', component: HomePageComponent},
+  { path: 'addData', component: AddDataComponent},
+  { path: 'viewData', component: ViewDataComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
